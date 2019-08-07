c8845d8c2ca1f0b34ad14c4f2b9c7ed8301e4f6d
Junior Fernandes <junior.fernandes@sti.ufc.br>
2019-08-07 09:58:56 -0300

    finalizando modulo 6

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">{{ titulo }}</div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      titulo: "hello world"
+    }
+  }
+}
+</script>
+
+<style>
+  #app{
+    background-color: chocolate;
+    color: #fff;
+  }
+</style>
+
