34509e7e929abcb335206484ef8e60a6194044d4
Junior Fernandes <junior.fernandes@sti.ufc.br>
2019-09-03 10:43:16 -0300

    conclusao modulo 13

---
+++ filtros-mixins/filtros-mixins-exercicios/src/usuarioMixin.js
@@ -0,0 +1,10 @@
+export default{
+    computed: {
+        usuarioLogado(){
+            return 'Junior Fernandes'
+        }
+    },
+    created(){
+        console.log('Created - Usuário Mixin!')
+    }
+}
