f292343a2625916a95e2b6fd2eb400d959b4fb2d
Junior Fernandes <junior.fernandes@sti.ufc.br>
2019-09-30 10:43:55 -0300

    aula 255 finalizada

---
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="usuario">
+        <h2>Usuários</h2>
+        <hr>
+        <p><strong>Código: </strong>{{ id }}</p>
+        <button sucesso @click="irParaInicio()">Voltar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['id'],
+    // data(){
+    //     return {
+    //         id: this.$route.params.id
+    //     }
+    // },
+    // watch: {
+    //     $route(to, from){
+    //         this.id = to.params.id;
+    //     }
+    // },
+    methods: {
+        irParaInicio(){
+            // this.$router.push('/')
+            this.$router.push({path: "/"})
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
