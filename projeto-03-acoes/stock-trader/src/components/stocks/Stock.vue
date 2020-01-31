96a8299189b6012919cdff84d60df396ef51d582
Junior Fernandes <junior.fernandes@sti.ufc.br>
2020-01-31 14:28:30 -0300

    aula 304

--- projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -1,10 +1,40 @@
 <template>
-  <h1>Ação</h1>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>{{stock.name}} <small>(Preço: {{stock.price}}</small></strong>
+      </v-card-title>
+    </v-card>
+    <v-car>
+      <v-container fill-height>
+        <v-text-field label="Quantidade" type="number" 
+          v-model.number="quantity"/>
+        <v-btn class="green darken-3 while--text"
+          :disabled="quantity<=0 ||!Number.isInteger(quantity)"
+          @click="buyStock">Comprar</v-btn>
+      </v-container>
+    </v-car>
+  </v-flex>
 </template>
 
 <script>
 export default {
-    
+    props: ['stock'],
+    data(){
+      return{
+        quantity: 0
+      }
+    },
+    methods: {
+      buyStock(){
+        const order = {
+          stockId: this.stock.id,
+          strockPrice: this.stock.price,
+          quantity: this.quantity
+        }
+        this.quantity=0
+      }
+    },
 }
 </script>
 
---
+++ projeto-03-acoes/stock-trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default[
+    {id:1, name: 'BMW', price: 50},
+    {id:2, name: 'Twitter', price: 20},
+    {id:3, name: 'Google', price: 100},
+    {id:4, name: 'Apple', price: 110},
+]
