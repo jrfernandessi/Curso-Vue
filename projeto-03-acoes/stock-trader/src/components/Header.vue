7074e069558988d7c6bd8c3b2aedece8769a22a6
Junior Fernandes <junior.fernandes@sti.ufc.br>
2020-02-06 08:09:10 -0300

    aula 310

--- projeto-03-acoes/stock-trader/src/components/Header.vue
+++ projeto-03-acoes/stock-trader/src/components/Header.vue
@@ -24,7 +24,7 @@
         </v-list-tile>
       </v-menu>
       <v-layout align-center>
-        <span class="text-uppercase grey--text text--darken-2">Saldo: {{ funds }}</span>
+        <span class="text-uppercase grey--text text--darken-2">Saldo: {{ funds | currency }}</span>
       </v-layout>
     </v-toolbar-items>
   </v-toolbar>
